8fca521b51155126d417d282b43750732c49ae3d
behzad <behzad.m.777666@gmail.com>
2015-08-10 09:40:03 +0430

    first commit

---
+++ Assets/Spawner.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour {
+
+	//Groups
+	public GameObject[] groups;
+
+	public void spawnNext() {
+		// Random Index
+		int i = Random.Range(0, groups.Length);
+		
+		// Spawn Group at current Position
+		Instantiate(groups[i],
+		            transform.position,
+		            Quaternion.identity);
+	}
+
+	// Use this for initialization
+	void Start () {
+		//print ("hi");
+		// Spawn initial Group
+		spawnNext();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
